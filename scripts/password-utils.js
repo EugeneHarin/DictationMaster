f80d232bba589e340402ce9b72586f02456d8526
Eugene Garin <diterecs@gmail.com>
2024-01-01 18:13:20 -0500

    fix password-utils
    add back the function used in seed.js

--- scripts/password-utils.js
+++ scripts/password-utils.js
@@ -1,11 +1,11 @@
-const { scryptSync, timingSafeEqual } = require('crypto');
+const { scryptSync, timingSafeEqual, randomBytes } = require('crypto');
 
-// const hashPassword = (password) => {
-//   const salt = randomBytes(16).toString('hex');
-//   const hashedPassword = scryptSync(password, salt, 64, { N: 16384, r: 8, p: 1 }).toString('hex');
-//   const hash = `${salt}:${hashedPassword}`;
-//   return hash;
-// }
+const hashPassword = (password) => {
+  const salt = randomBytes(16).toString('hex');
+  const hashedPassword = scryptSync(password, salt, 64, { N: 16384, r: 8, p: 1 }).toString('hex');
+  const hash = `${salt}:${hashedPassword}`;
+  return hash;
+}
 
 const verifyPassword = (inputPassword, storedPassword) => {
   const [salt, key] = storedPassword.split(':');
@@ -16,4 +16,4 @@ const verifyPassword = (inputPassword, storedPassword) => {
   else return false;
 }
 
-module.exports = { verifyPassword };
+module.exports = { hashPassword, verifyPassword };
